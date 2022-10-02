de670b3b82ad2c2aec3865e7a1ffd092fc80bdc0
FranciscoTurro <franciscogtrro@outlook.com.ar>
2022-10-02 18:03:15 -0300

    writing tests for ship

---
+++ tests/ship.test.js
@@ -0,0 +1,11 @@
+test('Ship has length', () => {
+  expect(ship(4).length).toBe(4);
+});
+
+test('Ship can be hit', () => {
+  expect().toBe();
+});
+
+test('Ship can be sunk', () => {
+  expect().toBe();
+});
